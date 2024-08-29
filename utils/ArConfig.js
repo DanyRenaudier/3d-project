d9c3b4ed375dbbac695e96db571309cd9a9e1b57
Dany Renaudier <dani11_21@hotmail.com>
2024-08-29 16:57:13 -0300

    xr supported by browser check added

--- utils/ArConfig.js
+++ utils/ArConfig.js
@@ -12,22 +12,22 @@ class XrConfig {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
         this.light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 3);
-        
+
         this.light.position.set(0.5, 1, 0.25);
-        
+
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.xr.enabled = true;
         document.body.appendChild(this.renderer.domElement);
-        
+
         this.sceneAdd([this.light, this.camera]);
-        
+
         this.addButton();
         this.xrSessionCheck();
     }
 
-    sceneAdd(add){
-        add.map(x=> this.scene.add(x));
+    sceneAdd(add) {
+        add.map(x => this.scene.add(x));
     }
 
     addButton() {
@@ -36,8 +36,12 @@ class XrConfig {
     }
 
     async xrSessionCheck() {
-        let button = document.getElementById('ARButton')
-        await navigator.xr.isSessionSupported('immersive-ar') ? button.innerHTML = 'Augmented Reality' : button.innerHTML = "AR NOT Supported"
+        if (!'xr' in window.navigator) {
+            return false
+        } else {
+            let button = document.getElementById('ARButton')
+            await navigator.xr.isSessionSupported('immersive-ar') ? button.innerHTML = 'Augmented Reality' : button.innerHTML = "AR NOT Supported"
+        }
     }
 }
 
