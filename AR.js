e9bba33894feecda6dbb8a553fe106310967cbf0
Dany Renaudier <dani11_21@hotmail.com>
2024-08-29 16:42:45 -0300

    xrSession created

--- AR.js
+++ AR.js
@@ -1,10 +1,3 @@
 import {XrConfig} from './utils/ArConfig'
-import { displayer } from './web';
 
-const xrSession = new XrConfig();
-
-const main=async()=>{
-    const result = await displayer.gltfLoader()
-}
-
-main()
+let xrSession = new XrConfig();
