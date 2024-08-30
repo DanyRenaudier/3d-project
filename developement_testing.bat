6f6cc912acdc1c9e91b74819f2257afc7f69416a
Dany Renaudier <dani11_21@hotmail.com>
2024-08-30 01:38:48 -0300

    deleted the useless env in cmd

--- developement_testing.bat
+++ developement_testing.bat
@@ -1,3 +1,3 @@
 call npm install
 start npm run start
-start cmd /c "timeout /t 2 && set /p port=<.env && start chrome https://192.168.1.9:3000/"
+start cmd /c "timeout /t 2 && start chrome https://192.168.1.9:3000/"
